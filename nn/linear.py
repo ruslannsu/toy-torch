80cba314eb2ffddb3b776d40f5d4676a6b39e9fe
ruslan <ruslanone57@gmail.com>
2026-04-07 01:00:47 +0700

    fix: linear, add: parameter

--- nn/linear.py
+++ nn/linear.py
@@ -1,22 +1,18 @@
 import numpy as np
 
 class Linear:
-    def __init__(self, input_size: int, output_size: int, activation=None) -> None:
+    def __init__(self, input_size: int, output_size: int) -> None:
         self.parameters = np.random.default_rng().normal(size=(output_size, input_size))
         self.input_size = input_size 
         self.output_size = output_size
-        self.activation = activation
-        self.d_activation = np.eye(self.input_size) if activation is None else activation.diff
-         
-    def forward(self, x: np.array):
+
+    def forward(self, x: np.array) -> np.array:
         self.input = x
-        return self.parameters @ x
+        return x @ self.parameters.T
 
-    def backward(self, pred_backward: np.array):
-        '''no graph implementation'''
-        self.grad = np.outer(pred_backward, self.d_activation @ self.input)
-        self.backward_grad =  pred_backward @ self.parameters #надо добавить дифференциал активатора
-
+    def backward(self, pred_backward: np.array) -> np.array:
+        self.grad = np.outer(pred_backward, self.input)
+        return pred_backward @ self.parameters
+    
     def __call__(self, x: np.array):
         return self.forward(x)
-
--- test.py
+++ test.py
@@ -1,26 +1,15 @@
+
 import numpy as np
+
 import nn
 
-loss = nn.MSE()
-model = nn.Linear(5, 2)
 
-X = np.array([1, 3, 4, 5 ,6, 6, 10])
+model = nn.Linear(5, 6)
 
-target = np.array([3, 5])
-eta = 0.0001
+z  = np.eye(5)
 
-model_1 = nn.Linear(7, 5)
+print(model(z))
 
-for epoch in range(1000):
-    out = model_1(X)
-    out  = model(out)
-    loss(y=out, target=target)
-    print(loss.item(), out)
-    loss.backward()
-    loss_backward = loss.backward_pass
-    model.backward(loss_backward)
-    model_1.backward(model.backward_grad)
-    #model.parameters = model.parameters - eta * model.grad
-    model_1.parameters = model_1.parameters - eta * model_1.grad
+k = np.eye(6)
 
-
+model.backward(k)
