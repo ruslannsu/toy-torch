0004424bf68926a40433748df47016c8b65bcef0
ruslan <ruslanone57@gmail.com>
2026-04-18 19:46:03 +0700

    fix: garb

--- toytorch/nn/module.py
+++ toytorch/nn/module.py
@@ -3,6 +3,7 @@ from .parameter import Parameter
 class Module:
     def __init__(self) -> None:
         self.parameter = Parameter([self, None])
+        
     def forward(self, x):
         pass
 
--- toytorch/nn/parameter.py
+++ toytorch/nn/parameter.py
@@ -4,6 +4,7 @@ import numpy as np
 class Parameter:
     layers = []
     calling = {}
+    
     def __init__(self, data: list):
         Parameter.calling[data[0]] = data[1]
         Parameter.layers.append(data[0])
